6b555713e3b26502456f914eacc02e02d13c41d7
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-12-20 11:33:41 +0600

    'initial commit'

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
+      useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
@@ -13,10 +17,10 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API is Running...'));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
