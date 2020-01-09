15c88d64f60a05ee357a88f55ce1f44b2fce7206
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-01-09 23:37:30 +0600

    Register and login done.

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onchangeHandler = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmitHandler = async e => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const body = JSON.stringify(formData);
+
+    try {
+      const res = await axios.post('/api/auth', body, config);
+
+      console.log(res.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="alert alert-danger">Invalid credentials</div>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onchangeHandler}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onchangeHandler}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
